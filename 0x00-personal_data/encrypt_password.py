1cc9213f8631aa034363c35cf413aab3851d5e85
deantosh <deantoshdaiddoh@gmail.com>
2024-11-09 09:41:45 +0000

    task 6: solution added

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """
+Task 5:;
 Implement a hash_password function that expects one string argument name
 password and returns a salted, hashed password, which is a byte string.
 
 Use the bcrypt package to perform the hashing (with hashpw).
+
+Task 6:
+Implement an is_valid function that expects 2 arguments and returns a boolean.
+
+Arguments:
+ - hashed_password: bytes type
+ - password: string type
+
+Use bcrypt to validate that the provided password matches the hashed password.
 """
 import bcrypt
 
@@ -22,6 +32,20 @@ def hash_password(name: str) -> bytes:
 
     # Generate salt and hash the password
     salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password, salt)
+    hash_password = bcrypt.hashpw(password, salt)
 
-    return hashed_password
+    return hash_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if passweord and hashed_password match
+
+    Args:
+        hashed_password - encrypted password
+        password - Plain password (string)
+
+    Returns:
+        True: Match
+        False: Not a  match
+    """
+    return bcrypt.checkpw(password.encode('UTF-8'), hashed_password)
