f1e19484f030d7acf88e0e535863af2404131eb6
deantosh <deantoshdaiddoh@gmail.com>
2024-11-11 19:37:50 +0000

    Auth Class: require_auth method implemented

--- 0x01-Basic_authentication/simpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/simpleAPI/api/v1/auth/auth.py
@@ -11,7 +11,19 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Not implemented"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # format path and add ending / if no found
+        if not path.endswith('/'):
+            path = path + '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Not implemented"""
