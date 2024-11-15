e6522204f38b71dde77ae1921d11cfa1185c0b55
deantosh <deantoshdaiddoh@gmail.com>
2024-11-15 16:59:12 +0000

    task 8: new route /api/v1/auth_session/logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -45,3 +45,16 @@ def user_login():
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_user_session():
+    """Deletes user session"""
+    from api.v1.app import auth
+
+    deleted_session = auth.destroy_session(request)
+    if not deleted_session:
+        abort(404)
+
+    return jsonify({}), 200
