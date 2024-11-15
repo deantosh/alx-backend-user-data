e0bc36791d621d872516ffbb41cd216a0aeaa99d
deantosh <deantoshdaiddoh@gmail.com>
2024-11-15 16:58:01 +0000

    task 8: added new method destroy_session - deletes the user session / logout

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -47,3 +47,23 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
 
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Deletes the user session/logout"""
+        if request is None:
+            return False
+
+        # Get session_id
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        # Get user id using the session_id
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+
+        # Delete session
+        del user_id_by_session_id[session_id]
+
+        return True
