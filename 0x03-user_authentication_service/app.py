659943a5d1003de9522eaebdafbb4fdd6dc443d5
deantosh <deantoshdaiddoh@gmail.com>
2024-11-21 14:05:26 +0000

    task 7: route to create new users added

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,17 +2,37 @@
 """
 Simple basic application
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, request, jsonify
 
 
-app = Flask()
+AUTH = Auth()
+app = Flask(__name__)
 
 
-@app.route("/", methods=["GE"], strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def index_page():
     """Default route"""
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def register_user():
+    """Creates a new user"""
+
+    # Get email and password
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        # Attempt to register a user
+        user = AUTH.register_user(email, password)
+        return jsonify({
+            "email": user.email, "message": "user created"})
+    except ValueError:
+        # If user is already registered
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
