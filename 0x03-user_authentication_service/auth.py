7425cfe59770267d9f86acfe40e8e579cd426789
deantosh <deantoshdaiddoh@gmail.com>
2024-11-21 18:59:18 +0000

    task 12: get_user_from_session_id method added to uth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,3 +87,16 @@ class Auth:
 
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(
+            self, session_id: str) -> Optional[User]:
+        """Retrieves user based on his/her session id"""
+
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
