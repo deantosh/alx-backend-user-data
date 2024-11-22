8856ff63788931079c13b2056537d92f9465b850
deantosh <deantoshdaiddoh@gmail.com>
2024-11-22 02:44:57 +0000

    route: updated

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,7 +49,7 @@ def login():
 
         response = make_response(
             jsonify({"email": email, "message": "logged in"}))
-        response.set_cookie('session_id', ssession_id)
+        response.set_cookie('session_id', session_id)
 
         return response
 
@@ -61,7 +61,7 @@ def login():
 def logout_user():
     """Logout user"""
     # Get session id from cookie
-    session_id = request.cokkies.get('session_id')
+    session_id = request.cookies.get('session_id')
 
     # Get user from session id
     user = AUTH.get_user_from_session_id(session_id)
