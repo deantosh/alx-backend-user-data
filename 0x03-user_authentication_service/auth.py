d190addc578dce2048c3ef72a3a8ec42033b5649
deantosh <deantoshdaiddoh@gmail.com>
2024-11-22 18:13:08 +0000

    task 18: update_password method added

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -128,3 +128,20 @@ class Auth:
 
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update user password"""
+
+        try:
+            # Get user by reset_token
+            user = self._db.find_user_by(reset_token=reset_token)
+
+            # Hash password
+            hashed_password = _hash_password(password)
+
+            # Update user password
+            self._db.update_user(
+                hashed_password=hashed_password, reset_token=None)
+
+        except NoResultFound:
+            raise ValueError
