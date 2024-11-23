1f78c61f5f722e61a4756559349fadc168718832
deantosh <deantoshdaiddoh@gmail.com>
2024-11-23 19:39:20 +0000

    get_reset_password_token method updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -122,7 +122,7 @@ class Auth:
             reset_token = _generate_uuid()
 
             # Update current user reset_token
-            self._db.update_user(reset_token=reset_token)
+            self._db.update_user(user.id, reset_token=reset_token)
 
             return reset_token
 
